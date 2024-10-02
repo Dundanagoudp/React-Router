e223b77aa0cd16aa2e535d841d015089d7fdab7b
chetanv18~ <chetan@gully2global.com>
2024-10-02 22:53:12 +0530

    new

--- react_router/src/Components/layout/Header.jsx
+++ react_router/src/Components/layout/Header.jsx
@@ -18,10 +18,21 @@ export const Header = () => {
                         <div className="head_txt">
                             <p>My First project in React</p>
                         </div>
-                        <div className="sign_in_up">
-                            <NavLink to="#">SIGN IN</NavLink>
-                            <NavLink to="#">SIGN UP</NavLink>
-                        </div>
+                        <div className="sign_in_up flex space-x-4">
+  <NavLink
+    to="#"
+    className="px-6 py-2 border-2 border-gray-300 rounded-lg text-white bg-gradient-to-r from-gray-700 to-black hover:from-black hover:to-gray-700 transition-all duration-300"
+  >
+    SIGN IN
+  </NavLink>
+  <NavLink
+    to="#"
+    className="px-6 py-2 border-2 border-gray-300 rounded-lg text-white bg-gradient-to-r from-gray-700 to-black hover:from-black hover:to-gray-700 transition-all duration-300"
+  >
+    SIGN UP
+  </NavLink>
+</div>
+
                     </div>
                 </section>
                 <div className="container">
@@ -35,7 +46,15 @@ export const Header = () => {
                                 height="50%"
                             />
                         </NavLink> */}
-                        <li className="nav-item">WorldKannda</li>
+<li className="nav-item">
+  <a
+    href="/"
+    className="relative px-6 py-2 text-xl font-bold text-white bg-gradient-to-r from-gray-900 to-gray-800 rounded-full shadow-lg border-2 border-transparent hover:border-red-500 hover:bg-gradient-to-r hover:from-red-500 hover:to-red-700 transition-all duration-500 ease-in-out transform hover:scale-105"
+  >
+    CHETAN
+    <span className="absolute inset-0 bg-gradient-to-r from-red-500 to-red-700 opacity-25 rounded-full blur transition-all duration-500"></span>
+  </a>
+</li>
                     </div>
                     <nav className="navbar">
                         <ul>
